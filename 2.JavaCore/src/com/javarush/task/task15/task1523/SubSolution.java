9c7733c4058f393f5a721172143518833b4cefd6
Sergei <myskov.s@gmail.com>
2017-06-15 22:16:58 +0300

    task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task15.task1523;
+
+/**
+ * Created by sergeymyskov on 15.06.17.
+ */
+public class SubSolution extends Solution {
+    SubSolution(String s1) {
+        super(s1);
+    }
+
+    public SubSolution(String s1, int i1) {
+        super(s1, i1);
+    }
+
+    protected SubSolution(String s1, int i1, short sh1) {
+        super(s1, i1, sh1);
+    }
+}
