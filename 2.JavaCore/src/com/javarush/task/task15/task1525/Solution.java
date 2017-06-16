5fbaf75b352f09c293273bf0c9ef62e7055c112e
Sergei <myskov.s@gmail.com>
2017-06-16 20:01:56 +0300

    task1525

--- 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task15.task1525;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +10,20 @@ import java.util.List;
 
 public class Solution {
     public static List<String> lines = new ArrayList<String>();
+
+    static {
+        try {
+            BufferedReader readerFile = new BufferedReader(new FileReader(new File(Statics.FILE_NAME)));
+            String line;
+            while ((line = readerFile.readLine()) != null) {
+                lines.add(line);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(lines);
--- 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -1,5 +1,5 @@
 package com.javarush.task.task15.task1525;
 
 public class Statics {
-    public static String FILE_NAME = /* add your path to source file here*/;
+    public static String FILE_NAME = "//Users//sergeymyskov//IdeaProjects//JavaRushTasks//2.JavaCore//src//com//javarush//task//task15//task1525//test.txt"; /* add your path to source file here*/;
 }
