5bc68f23f4be228f159a87f5cfeaa5627f85b55d
Sergei <myskov.s@gmail.com>
2017-06-17 22:34:32 +0300

    task1527

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -0,0 +1,67 @@
+package com.javarush.task.task15.task1527;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Парсер реквестов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String str;
+        String substr;
+        String[] parametrs;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            str = reader.readLine();
+            //отрезаем строку до знака "?" включительно
+            substr = str.substring(str.indexOf('?') + 1);
+
+            parametrs = substr.split("&");
+            for (String s: parametrs) {
+                if(s.contains("=")) {
+                    System.out.print(s.substring(0, s.indexOf('=')) + " ");
+                } else {
+                    System.out.print(s + " ");
+                }
+            }
+            System.out.println();
+
+            String subObj;
+            for (String ss: parametrs) {
+                if (ss.contains("obj=")) {
+                    subObj = ss.substring(ss.indexOf('=') + 1);
+                    try {
+                        alert(Double.parseDouble(subObj));
+                    } catch ( NumberFormatException e) {
+                        alert(subObj);
+                    }
+                }
+            }
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
+
+
+
+
+
+        //add your code here
+    }
+
+    public static void alert(double value) {
+        System.out.println("double " + value);
+    }
+
+    public static void alert(String value) {
+        System.out.println("String " + value);
+    }
+}
