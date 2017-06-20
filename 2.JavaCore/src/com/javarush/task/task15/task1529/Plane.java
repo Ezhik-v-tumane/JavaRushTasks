9c826e3d0906894364bab9bc0695cdf1fbd76858
Sergei <myskov.s@gmail.com>
2017-06-20 20:32:30 +0300

    task1529

--- 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -1,7 +1,7 @@
 package com.javarush.task.task15.task1529;
 
 /**
- * Created by sergeymyskov on 18.06.17.
+ * Created by sergeymyskov on 20.06.17.
  */
 public class Plane implements Flyable {
     public int countPass;
--- 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Solution.java
@@ -12,14 +12,33 @@ public class Solution {
     public static void main(String[] args) {
 
     }
-    
+
     static {
+        Solution.reset();
         //add your code here - добавьте код тут
     }
 
     public static Flyable result;
 
     public static void reset() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String resultReader = reader.readLine();
+
+            if (resultReader.equals("helicopter")) {
+                result = new Helicopter();
+            } else if (resultReader.equals("plane")) {
+                int countPass = Integer.parseInt(reader.readLine());
+                result = new Plane(countPass);
+            } else {
+                result = null;
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         //add your code here - добавьте код тут
     }
 }
