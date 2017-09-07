68bd0354f617807f983d002fc9d32f38bd837c98
Sergei <myskov.s@gmail.com>
2017-09-07 11:48:09 +0300

    task1804

--- 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1804/Solution.java
@@ -1,4 +1,4 @@
-package com.javarush.task.task18.task1803;
+package com.javarush.task.task18.task1804;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -6,12 +6,11 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /* 
-Самые частые байты
+Самые редкие байты
 */
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-
         ArrayList<Integer> arr = new ArrayList<Integer>();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -19,9 +18,10 @@ public class Solution {
         //String fileName = "/Users/sergeymyskov/IdeaProjects/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1803/1.txt";
         String fileName = reader.readLine();
 
-        int max = -2147483648;
+        int min = 2147483647;
+        int prvMin = 2147483647;
         int tmp;
-        String maxBite = "";
+        String minBite = "";
 
         FileInputStream file = new FileInputStream(fileName);
 
@@ -29,33 +29,40 @@ public class Solution {
             int intTmp = file.read();
             arr.add(intTmp);
         }
+
         for (int i = 0; i < arr.size(); i++){
             tmp = Collections.frequency(arr, arr.get(i)); //подсчитываем количество совпадений элемента в списке
-            if (tmp >= max){
-                max = tmp;
-                if (maxBite.equals("")){
-                    maxBite = String.valueOf(arr.get(i));
-                } else {
-                    maxBite = maxBite + " " + String.valueOf(arr.get(i));
+            if (min >= tmp){
+                min = tmp;
+                if (min < prvMin){
+                    minBite = String.valueOf(arr.get(i));
+                    prvMin = min;
+                } else if (min == prvMin){
+                    minBite = minBite + " " + String.valueOf(arr.get(i));
                 }
+
             }
         }
+
         reader.close();
         file.close();
-        String[] arrStr = maxBite.split(" ");
+
+        String[] arrStr = minBite.split(" ");
 
         Set<String> set = new HashSet<String>(Arrays.asList(arrStr));
         String[] result = set.toArray(new String[set.size()]);
 
-        maxBite = "";
+        minBite = "";
 
         for (int i = 0; i < result.length; i++){
             if (i == 0)
-                maxBite = result[i];
+                minBite = result[i];
             else
-                maxBite = maxBite + " " + result[i];
+                minBite = minBite + " " + result[i];
         }
 
-        System.out.println(maxBite);
+        System.out.println(minBite);
+
+
     }
 }
