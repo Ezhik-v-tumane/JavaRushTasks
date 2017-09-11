7e780db3a1796e0a72218400f7ba2fc63e75da70
Sergei <myskov.s@gmail.com>
2017-09-12 00:20:39 +0300

    task1821

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task18.task1822;
+
+/* 
+Поиск данных внутри файла
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int id = Integer.parseInt(args[0]);
+
+
+    }
+}
