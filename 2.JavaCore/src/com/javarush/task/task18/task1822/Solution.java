bf0825b2073dfab9dfda2451fb70c7ad7814f130
Sergei <myskov.s@gmail.com>
2017-09-13 22:10:37 +0300

    task1822

--- 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1822/Solution.java
@@ -4,10 +4,27 @@ package com.javarush.task.task18.task1822;
 Поиск данных внутри файла
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
-        int id = Integer.parseInt(args[0]);
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //String fileName = "/Users/sergeymyskov/IdeaProjects/JavaRushTasks/2.JavaCore/src/com/javarush/task/task18/task1822/1.txt";
+        String fileName = reader.readLine();
+
+        BufferedReader file = new BufferedReader(new FileReader(fileName));
+
+        while (file.ready()){
+            String tmp = file.readLine();
+            if (tmp.startsWith(args[0])) {
+                System.out.println(tmp);
+            }
+        }
+        file.close();
+        reader.close();
 
 
     }
