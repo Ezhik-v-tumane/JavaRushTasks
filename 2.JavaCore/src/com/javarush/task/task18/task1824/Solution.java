2f4ef72bab725b8a2c7c1644c7ac1569b3645db8
Sergei <myskov.s@gmail.com>
2017-09-25 21:01:52 +0300

    task1824

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -4,7 +4,23 @@ package com.javarush.task.task18.task1824;
 Файлы и исключения
 */
 
+import java.io.*;
+
 public class Solution {
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = null;
+        while (true){
+            try {
+                fileName = reader.readLine();
+                FileInputStream f = new FileInputStream(fileName);
+                f.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
