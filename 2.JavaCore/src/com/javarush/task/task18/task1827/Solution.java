c6b244c956f8f290c582783e37e210444ccc3629
Sergei <myskov.s@gmail.com>
2017-09-27 21:10:02 +0300

    task1827

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -1,10 +1,92 @@
 package com.javarush.task.task18.task1827;
 
+import java.io.*;
+import java.util.ArrayList;
+
 /* 
 Прайсы
 */
 
+//Можно сделать проверку на количество аргументов и корректность ввода даных, но я не уверен ,
+//что этот долбанутый валидадор пропустит это решение.
+
 public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String argument = args[0];
+        String productName = args[1];
+        String price = args[2];
+        String quanlity = args[3];
+        int max = -2147483648;
+
+        //для тестирования аргумент добавления строки
+        //String argument = "-c";
+        //для тестирования наименование товара 30 символов
+        //String productName = "хреньjsfhgjlksdfghljkdsfhgjk;lsdfhgjksdfhgfsdjlkhglsd";
+        //для тестирования цена товара 8 символов
+        //String price = "123456789.00";
+        //для тестирования количество товара 4 символа
+        //String quanlity = "34456";
+
+        if (productName.length() > 30)
+            productName = productName.substring(0,30);
+        if (price.length() > 8)
+            price = price.substring(0, 8);
+        if (quanlity.length() > 4)
+            quanlity = quanlity.substring(0, 4);
+
+        String fileName = reader.readLine();
+
+        //для тестирования путь к файлу
+        //String fileName = "C://Java//src//com//javarush//task//task18//task1827//1.txt";
+
+        ArrayList<String> arrStr = new ArrayList<>();
+
+
+        switch (argument){
+            case "-c": {
+                BufferedReader readerFile = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+
+                String strLine;
+
+                while ((strLine = readerFile.readLine()) != null){
+                    arrStr.add(strLine);
+                    if (Integer.parseInt(strLine.substring(0, 8).trim()) > max)
+                        max = Integer.parseInt(strLine.substring(0, 8).trim());
+                }
+                readerFile.close();
+
+                String id = Integer.toString(max + 1);
+
+                String text = String.format("%-8s", id) + String.format("%-30s",productName) + String.format("%-8s", price) + String.format("%-4s",quanlity);
+                arrStr.add(text);
+
+                BufferedWriter buf = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName)));
+                for (String s: arrStr)
+                    buf.write(s+"\r\n");
+
+                buf.close();
+
+
+                /*
+                // Тестовый блок для вывода промежуточных результатов
+                System.out.println("Вывод всех ID на печать");
+                for (int i = 0; i < arrStr.size(); i++){
+                    System.out.println(arrStr.get(i));
+                }
+                System.out.println("Максимально значение: " + max);
+                System.out.println("Максимально значение + 1: " + id);
+                System.out.println("Максимально значение + 1 и дополнинное пробелами: " + id8 + "!");
+                System.out.println("Итого строка: " + text + "!");
+                System.out.println("Длина строки: " + text.length());
+                */
+
+            }
+        }
+
+        reader.close();
+
     }
 }
