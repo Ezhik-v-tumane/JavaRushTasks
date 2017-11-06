1d2db139685a0791676addabb93c12b878bea9fd
Sergei <myskov.s@gmail.com>
2017-11-06 22:14:27 +0300

    task2001&2004

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task19.task1918;
+
+/* 
+Знакомство с тегами
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+
+        BufferedReader file = new BufferedReader(new FileReader(fileName));
+
+
+    }
+}
