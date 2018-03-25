1644f1ef45f72ff3f3837f0ed141ca24b33a67b1
Sergei <myskov.s@gmail.com>
2018-03-25 17:36:13 +0300

    task2209

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,50 +19,38 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         //String file = bufferedReader.readLine();
         String file = "/Users/sergeymyskov/IdeaProjects/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2209/test.txt";
+        Scanner in = new Scanner(new File(file));
 
-        String content = new String(Files.readAllBytes(Paths.get(file)), StandardCharsets.UTF_8);
-        String[] words = content.split(" ");
+        ArrayList<String> wordsFromFile = new ArrayList<>();
+
+        while (in.hasNext()){
+            wordsFromFile.add(in.next());
+        }
+        Collections.sort(wordsFromFile);
+
+        String[] words = wordsFromFile.toArray(new String[wordsFromFile.size()]);
 
 
         StringBuilder result = getLine(words);
         System.out.println(result.toString());
 
         bufferedReader.close();
+        in.close();
     }
 
     public static StringBuilder getLine(String... words) {
         StringBuilder result = new StringBuilder();
-        if (words == null || words.length == 0)
-            return result;
-        if (words.length==1 || words[0].equals(""))
-            return result.append(words[0]);
+        if (words == null || words.length == 0) return result;
+        if (words.length==1 || words[0].equals("")) return result.append(words[0]);
 
-        ArrayList<String> wordsFromFile = new ArrayList<>();
-        wordsFromFile.addAll(Arrays.asList(words));
+        ArrayList<String> wordsList = new ArrayList<>();
 
-        while (wordsFromFile.remove("")){
-            wordsFromFile.remove("");
-        }
-        while (isYes(wordsFromFile)) {
-            Collections.shuffle(wordsFromFile);
-        }
-        for (String word: wordsFromFile){
-            result.append(word).append(" ");
-        }
-        result.deleteCharAt(result.length()-1);
+        wordsList.addAll(Arrays.asList(words));
+
+
 
 
 
         return result;
-
-    }
-
-    public static boolean isYes(ArrayList<String> wordsFromFile) {
-        for (int i = 0; i < wordsFromFile.size() - 1; i++) {
-            String firstWord = wordsFromFile.get(i).toLowerCase();
-            String secondWord = wordsFromFile.get(i + 1).toLowerCase();
-            if (firstWord.charAt(firstWord.length() - 1) != secondWord.charAt(0)) return true;
-        }
-        return false;
     }
 }
